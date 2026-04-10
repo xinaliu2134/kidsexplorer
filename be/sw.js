52d351c069ac693610bc3cad7b3851789aa47641
Nana <nana@Nanas-MacBook-Air-2.local>
2026-04-10 15:11:29 +0200

    feat: PWA support + improved mobile empty state (Near Me btn, half-drawer on load)

---
+++ be/sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'kidsexplorer-v1';
+const CACHED = ['./'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CACHED)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match('./')));
+  }
+});
